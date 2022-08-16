a3978b67de8615d37a63beaebd1b6eed1a5b3e44
Griffin Moyer <moyerga@outlook.com>
2022-08-16 10:15:31 -0500

    fixed response from register_user method to complete logic on client side

--- app_api/views/auth.py
+++ app_api/views/auth.py
@@ -53,5 +53,5 @@ def register_user(request):
     token = Token.objects.create(user=new_user)
     # TODO: If you need to send the client more information update the data dict
     
-    data = { 'token': token.key, 'user_id': new_user.id }
+    data = { 'token': token.key, 'user_id': new_user.id, 'valid': True}
     return Response(data, status=status.HTTP_201_CREATED)
